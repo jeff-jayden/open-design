76f24bd8485752959a51300f19ebc0c168367d6b
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-04-14 16:16:21 +0800

    feat: 组件页面

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 const opDescription = '一个包含通用组件的库'
@@ -6,28 +6,64 @@ const opTitle = 'Open-Design'
 
 
 export default defineConfig({
-  title: opTitle,
-  description: opDescription,
-  head: [['link']]
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: '主页', link: '/' },
-      { text: '组件', link: '/markdown-examples' }
-    ],
+    title: opTitle,
+    description: opDescription,
+    lastUpdated: true,
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+    head: [['link', {rel: "icon", href: "/snowflake.png"}]],
+
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        logo: '/snowflake.png',
+        search: {
+            provider: "local",
+            options: {
+                locales: {
+                    zh: {
+                        translations: {
+                            button: {
+                                buttonText: "搜索文档",
+                                buttonAriaLabel: "搜索文档",
+                            },
+                            modal: {
+                                noResultsText: "无法找到相关结果",
+                                resetButtonTitle: "清除查询条件",
+                                footer: {
+                                    selectText: "选择",
+                                    navigateText: "切换",
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+
+        editLink: {
+            pattern: 'https://github.com/jeff-jayden/open-design.git',
+            text: '为此页提供修改建议'
+        },
+
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '组件', link: '/components/index', activeMatch: '/components/'},
+            {text: '指南', link: '/guide/install', activeMatch: '/guide/'},
+        ],
+
+        sidebar: {
+            "/guide/": [
+                {
+                    text: "指南",
+                    items: [
+                        {text: "安装", link: "/guide/install"},
+                        {text: "快速开始", link: "/guide/quickstart"},
+                    ],
+                },
+            ],
+        },
+
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/jeff-jayden/open-design.git'}
         ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    }
 })
