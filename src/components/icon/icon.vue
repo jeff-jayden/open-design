4b6d50af8a2baed5037f0aab1d034991c521a3a3
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-04-14 23:04:38 +0800

    icon和button组件

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,25 @@
+<template>
+  <i class="open-icon" :class="{[`open-icon--${type}`]:type}" :style="customStyles" v-bind="$attrs">
+    <font-awesome-icon v-bind="filterProps"/>
+  </i>
+</template>
+
+<script setup lang="ts">
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import type {IconProps} from './types'
+import {omit} from 'lodash-es';
+import {computed} from 'vue';
+
+defineOptions({
+  name: 'OpenIcon',
+  inheritAttrs: false
+})
+
+const props = defineProps<IconProps>()
+const filterProps = computed(() => omit(props, ['type', 'color']))
+const customStyles = computed(() => props.color ? {color: props.color} : {})
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
-import './assets/main.css'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import './styles/index.css'
 
+library.add(fas)
 const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
