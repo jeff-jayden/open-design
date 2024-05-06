fde0a6451aa7936a6b3e0f9c2e931a47d682004b
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-05-06 23:14:43 +0800

    feat: 修改icon组件&完成input组件

--- src/components/icon/icon.vue
+++ src/components/icon/icon.vue
@@ -1,13 +1,17 @@
 <template>
-  <i class="open-icon" :class="{[`open-icon--${type}`]:type}" :style="customStyles" v-bind="$attrs">
-    <font-awesome-icon v-bind="filterProps"/>
+  <i
+      class="open-icon"
+      :class="{
+        'loading': loading
+      }"
+      :style="customStyles"
+      v-bind="$attrs">
+    <slot/>
   </i>
 </template>
 
 <script setup lang="ts">
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import type {IconProps} from './types'
-import {omit} from 'lodash-es'
 import {computed} from 'vue'
 
 defineOptions({
@@ -16,7 +20,6 @@ defineOptions({
 })
 
 const props = defineProps<IconProps>()
-const filterProps = computed(() => omit(props, ['type', 'color']))
 const customStyles = computed(() => props.color ? {color: props.color} : {})
 </script>
 
--- src/components/input/types.ts
+++ src/components/input/types.ts
@@ -1,14 +1,20 @@
+
 export interface InputProps {
     type?: string;
-    prefixIcon?: string;
-    minlength?: number;
-    maxlength?: number;
-    placeholder?: string;
+    modelValue: string;
+    size?: 'large' | 'small';
+    /**是否禁用input输入框*/
+    disabled?: boolean;
+    /**是否开启一键清空*/
+    clearable?: boolean;
+    /**是否开启密码显示*/
     showPassword?: boolean;
+    placeholder?: string;
+    readonly?: boolean;
+    autocomplete?: string;
     autofocus?: boolean;
+    form?: string;
 }
-
-
 export interface InputEmits {
     /**支持v-model而添加的事件*/
     (e: 'update:modelValue', value: string) : void;
@@ -19,4 +25,8 @@ export interface InputEmits {
     (e: 'focus', value: FocusEvent): void;
     (e: 'blur', value: FocusEvent): void;
     (e: 'clear'): void;
+}
+
+export interface InputInstance {
+    ref: HTMLInputElement | HTMLTextAreaElement;
 }
