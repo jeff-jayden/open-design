acf7e4aeaa1a3b74e8bfddb0231b9910d7599208
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-05-07 13:34:26 +0800

    feat: 完成select组件

--- src/components/select/type.ts
+++ src/components/select/type.ts
@@ -1,13 +1,28 @@
 import type {UPDATE_MODEL_EVENT} from '@/constants'
+export interface SelectOption {
+    label: string;
+    value: string;
+    disabled?: boolean;
+}
 
+export interface SelectStates {
+    inputValue: string;
+    selectedOption: null | SelectOption;
+    mouseHover: boolean;
+    loading: boolean;
+    highlightIndex: number;
+}
 
 export interface OpenSelectProps {
     modelValue?: 'string' | 'number' | 'boolean' | 'object'
     disabled?: boolean;
     effect?: 'dark' | 'light';
+    options?: SelectOption[];
 }
 
 export interface OpenSelectEmits {
-    // (e: UPDATE_MODEL_EVENT, value: string): void,
+    (e: 'update:modelValue', value: string): void,
+    (e: 'visible-change', value: boolean): void,
+    (e: 'change', value: string): void,
 
 }
