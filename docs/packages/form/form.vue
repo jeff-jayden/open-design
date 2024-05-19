df54f1bd631fe44279aad71e56c7d19a9fb69326
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-05-19 21:43:17 +0800

    feat: form

--- docs/packages/form/form.vue
+++ docs/packages/form/form.vue
@@ -9,7 +9,18 @@
         show-message
         prop="name"
     >
-      <OpenInput id="hello" v-model="form.name"/>
+      <open-input id="name" v-model="form.name"/>
+    </open-form-item>
+    <open-form-item
+        label="Activity region"
+        show-message
+        prop="region"
+    >
+      <open-select
+          v-model="form.region"
+          placeholder="Select"
+          :options="options"
+      />
     </open-form-item>
   </open-form>
 </template>
@@ -20,6 +31,7 @@ import OpenForm from "@/components/form/form.vue";
 import OpenInput from "@/components/input";
 import OpenFormItem from "@/components/form/form-item.vue";
 import {reactive} from "vue";
+import OpenSelect from "@/components/select";
 
 
 const form = reactive({
@@ -33,10 +45,38 @@ const form = reactive({
   desc: '',
 })
 
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+    disabled: true
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+]
+
 const rules = reactive({
   name: [
     { required: true, message: 'Please input Activity name', trigger: 'blur' },
     { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+  ],
+  region: [
+    { required: true, message: 'Please input Activity region', trigger: 'blur' }
   ]
 })
 </script>
