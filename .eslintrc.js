a7fa8e0b65f60e71e2b2d98270ea37c221aa25f7
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-08-11 16:23:22 +0800

    feat: 修改eslint提交规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,37 @@
 module.exports = {
   root: true,
   parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true
+  },
+  plugins: ['vue'],
   extends: [
+    "airbnb",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     "plugin:prettier/recommended"
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    sourceType: "module",
+    ecmaVersion: '6'
+  },
+  rules: {
+    "indent": [
+      "error",
+      4
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
   }
 }
