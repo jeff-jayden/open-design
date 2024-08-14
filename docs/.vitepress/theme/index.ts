b50dc934633d9dc806a8761a867e4f6b3d3841e7
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-08-14 23:33:43 +0800

    feat: 修改文件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,14 +2,11 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {fas} from '@fortawesome/free-solid-svg-icons'
 import { ElementPlusContainer } from '@vitepress-demo-preview/component'
 import '@vitepress-demo-preview/component/dist/style.css'
 import '../../../src/styles/index.css'
 import "./style.css";
 import './custom.css'
-library.add(fas)
 
 export default {
   extends: DefaultTheme,
