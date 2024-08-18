515c9c6f3a74a1499c1cacef2cf04cf065f073cf
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-08-18 11:41:43 +0800

    添加app

---
+++ app/ex-app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {OpenButton} from "@open-design/components/button";
+</script>
+
+<template>
+  <OpenButton type="primary">提交</OpenButton>
+</template>
+
+<style scoped>
+
+</style>
