72c5e68e177f50123a9e6bfe87e3238ba3ebfb87
jeff-jayden <115272196+jeff-jayden@users.noreply.github.com>
2024-10-05 11:43:36 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import '@qinloong/snow-sky/dist/index.css';
 import 'element-plus/dist/index.css';
 
 import ElementPlus from 'element-plus';
-import snowDesign from '@qinloong/snow-sky';
 import App from './App.vue';
 
 const app = createApp(App);
-app.use(snowDesign);
 app.use(ElementPlus);
 
 app.mount('#app');
