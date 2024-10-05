8f874ede0229e51e69a4eb37530310b604110d22
jeff-jayden <115272196+jeff-jayden@users.noreply.github.com>
2024-10-05 11:43:49 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import './styles/index.css';
-import '@qinloong/snow-sky/dist/index.css';
 import 'element-plus/dist/index.css';
 
 import ElementPlus from 'element-plus';
